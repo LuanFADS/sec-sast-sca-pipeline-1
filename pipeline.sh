510bb7c19c7a5b43a4dbf31043c58f0a4f8cd2d5
Rafael Rasso <rafael.rasso@gmail.com>
2021-10-21 01:30:01 +0000

    SCA Github action example

--- pipeline.sh
+++ pipeline.sh
@@ -1,23 +1,23 @@
-# Este arquivo é apenas uma demonstração de uma "pipeline" escrita com shell script, apenas para efeitos didáticos.
-# Este exemplo pode (e deve) ser enriquecido com mais detalhes, porém não fazem parte do escopo da disciplina. 
+# # Este arquivo é apenas uma demonstração de uma "pipeline" escrita com shell script, apenas para efeitos didáticos.
+# # Este exemplo pode (e deve) ser enriquecido com mais detalhes, porém não fazem parte do escopo da disciplina. 
 
-# 1. Limpar o ambiente
+# # 1. Limpar o ambiente
 mvn clean
 
-# 2. Instalar as dependencias
+# # 2. Instalar as dependencias
  mvn install -DskipTests
 
 
-# 3. Rodar os testes automatizados
+# # 3. Rodar os testes automatizados
  mvn test
 
 # 4. Rodar a analise do dependency track
 MVN_GROUPID="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.groupId}' exec:exec)"
 MVN_ARTIFACTID="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.artifactId}' exec:exec)"
 MVN_VERSION="$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' exec:exec)"
-curl -X "POST" "http://localhost:8081/api/v1/bom" \
+curl -X "POST" "https://8081-lavender-spoonbill-5f1p2o0b.ws-us17.gitpod.io/api/v1/bom" \
       -H 'Content-Type: multipart/form-data' \
-      -H 'X-API-Key: cwORXrehpxd7bG8dHpjfTYkJJziaQIW2 ' \
+      -H 'X-API-Key: 8kgEYuHFBySWoibLVhFpmyuipooGQiCb ' \
       -F "autoCreate=true" \
       -F "projectName=$MVN_GROUPID:$MVN_ARTIFACTID" \
       -F "projectVersion=$MVN_VERSION" \
